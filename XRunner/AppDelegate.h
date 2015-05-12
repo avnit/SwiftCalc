2366db65f86a2c7a4a6a3193a300282211ebd46b
Avnit Bambah <avnitbambah@Avnits-MacBook-Pro.local>
2015-05-12 13:29:03 -0500

    Initial Commit

---
+++ XRunner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XRunner
+//
+//  Created by Avnit Bambah on 5/12/15.
+//  Copyright (c) 2015 Avnit Bambah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
