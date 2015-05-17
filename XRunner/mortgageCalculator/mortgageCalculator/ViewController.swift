19199a792fac0ce7ed2eeadd209d1096ada432f0
Avnit Bambah <avnitbambah@Avnits-MacBook-Pro.local>
2015-05-17 13:06:02 -0500

    test code

--- XRunner/mortgageCalculator/mortgageCalculator/ViewController.swift
+++ XRunner/mortgageCalculator/mortgageCalculator/ViewController.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 
+
+
+
+var str = "Hello, playground"
+
+class calculator
+{
+    /*  let loanAmount: Double
+    let term: Double
+    let interestRate: Double
+    
+    func init (loanAmount: Double , term: Double , interestRate: Double)
+    {
+    self.loanAmount = loanAmount
+    self.term = term
+    self.interestRate = interestRate
+    println("We are in Init")
+    }
+    */
+    
+    func calculate( loanAmount: Double, term: Double, interestRate: Double ) ->Double
+    {
+        return ((loanAmount * term * interestRate) / 100) + loanAmount
+    }
+}
+
+var loanAmount: Double = 8000000.00
+var term: Double = 1.0
+var interestRate: Double = 9.0
+
+var a = calculator()
+var result: Double = a.calculate(loanAmount, term: term, interestRate: interestRate)
+
+
+
+
+
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
